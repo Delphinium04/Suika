59603d3db4918e5d1a9c96ef217edbf1b7b665c6
이유찬 <delphinium04@outlook.com>
2023-11-19 15:00:42 +0900

    Almost finish

--- SettingsForSuika.py
+++
@@ -1,4 +0,0 @@
-class Options():
-    def __init__(self) -> None:
-        self.screen_size = (800, 800)
-        self.fps = 165
--- Suika.py
+++ Suika.py
@@ -1,27 +1,18 @@
+import math
+import random
 import pygame as pg
 import pymunk as pm
 import pymunk.pygame_util
-import math
-import random
-import SettingsForSuika as user_settings
-from typing import List  # to show type hint in collision_ball_hit()
-from enum import IntEnum
+from pymunk import Vec2d
 
-SCREEN_SIZE = user_settings.Options().screen_size
-FPS = user_settings.Options().fps
-
-# https://www.pymunk.org/en/latest/pymunk.html Document
 
 '''
-수정 주의사항!
-수정할 일 있으면 수정하되 주석 처리한 링크는 맨 위로 올려서라도 살려두기
-레벨은 Color로 구분하지 않고 pymunk.Circle.radius를 통해 구분 (order_radius[] 와 get_next_radius() 사용하기 (최고레벨이면 -1 반환))
-
-Debug Draw -> PyGame Draw 전환: https://stackoverflow.com/questions/53023396/how-not-to-render-ball-orientation-in-pymunk-space-debug-draw
-
+Debug Draw: https://stackoverflow.com/questions/53023396/how-not-to-render-ball-orientation-in-pymunk-space-debug-draw
 '''
 
-# region GameSetting
+SCREEN_SIZE = (800, 800)
+PREVIEW_BALL_OFFSET_Y = 60
+FPS = 60
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -34,215 +25,297 @@ THISTLE = (255, 0, 255)
 HOTPINK = (255, 105, 180)
 PURPLE = (128, 0, 128)
 
-order_color = [WHITE, RED, ORANGE, YELLOW,
-               GREEN, BLUE, THISTLE, HOTPINK, PURPLE]
-order_score = [1, 2, 4, 8, 16, 32, 64, 128, 256]
-order_radius = [15, 22, 42, 55, 68, 85, 94, 110, 130]
+COLOR_ORDER: list = [WHITE, RED, ORANGE, YELLOW,
+                     GREEN, BLUE, THISTLE, HOTPINK, PURPLE]
+SCORE_ORDER: list = [1, 2, 4, 8, 16, 32, 64, 128, 256]
+RADIUS_ORDER: list = [15, 22, 29, 40, 55, 70, 85, 100, 120]
 
-# 확통 내용?
-skwed_probabitity = [0.7, 0.1, 0.05, 0.08, 0.04, 0.02, 0.01, 0, 0]
-score = 0
-
-# pygame init
-running = True
-game_over = False
+COOLDOWN_DURATION = 1000  # 0초 1000 = 1초
 
 
-class Ball():
-    """
-    클래스 생성 시 초기 위치, 반지름(초기값 고정), STATIC 여부 설정 가능\n
-    set_color로 색 설정 후 set_visible()로 space에 추가
-    """
-
-    def __init__(self, pos=(0, 0), radius=order_radius[0], is_static=False):
-        if is_static:
-            self.body = pm.Body(body_type=pm.Body.KINEMATIC)
-        else:
-            self.body = pm.Body(1, 1)
-        self.body.position = pos
-        self.shape = pm.Circle(
-            body=self.body, radius=radius, offset=(0, 0))
-        self.shape.elasticity = 0.5
-        self.shape.friction = 0.1
-        self.is_dropped = False
-        self.shape.color = (255, 255, 255, 255)
-
-    def set_color(self, rgb: tuple):
-        color = (rgb[0], rgb[1], rgb[2], 255)
-        self.shape.color = color
-
-    def set_position(self, pos: tuple):
-        self.body.position = pos
-
-    def set_visible(self):
-        space.add(self.body, self.shape)
-        if self.body.body_type != pm.Body.KINEMATIC:
-            print(f'Level {order_radius.index(
-                self.shape.radius)} ball Set visible')
-
-    def get_position(self) -> tuple:
-        return self.body.position
+def get_next_radius(cur_radius):
+    ''' return -1 if radius is max level '''
+    if cur_radius == RADIUS_ORDER[-1]:
+        return -1
+    index = RADIUS_ORDER.index(cur_radius)
+    return index + 1
 
 
-def get_next_radius(current_radius) -> int:
-    next_idx = order_radius.index(current_radius) + 1
-    return next_idx if next_idx < len(order_radius) else -1
+def flipy(p):
+    """Convert chipmunk physics to pygame coordinates."""
+    return Vec2d(p[0], -p[1]+800)
 
 
-def remove_ball(*args: Ball):
-    if len(args) == 0:
-        return
-    for ball in args:
-        if ball in balls:
-            balls.remove(ball)
-        space.remove(ball.shape, ball.body)
+class Game:
+    class Ball(pg.sprite.Sprite):
 
+        def __init__(self, pos, radius, color, space, is_static=False):
+            super().__init__()
+            # args
+            self.color = pg.Color(color)
 
-pg.init()
-screen = pg.display.set_mode(SCREEN_SIZE)
-clock = pg.time.Clock()
+            self.origin_image = pg.Surface(
+                (radius*2+2, radius*2+2), pg.SRCALPHA)
+            self.image = self.origin_image
 
-random_index = range(len(order_score))
-random_int = random.choices(random_index, weights=skwed_probabitity, k=1)[0]
-next_ball_radius = order_radius[random_int]
-next_ball_color = order_color[random_int]
+            pg.draw.circle(self.image, self.color,
+                           (radius+1, radius+1), radius)
+            self.rect = self.image.get_rect(topleft=pos)
 
-# physics init
-space = pm.Space()
-space.gravity = (0, 1000)
-draw_options = pymunk.pygame_util.DrawOptions(screen)
+            if is_static:
+                self.body = pm.Body(body_type=pm.Body.KINEMATIC)
+            else:
+                self.body = pm.Body(10000000, 1)
+                self.body.velocity_func = Game.custom_gravity
 
-wall_bottom = pm.Body(body_type=pm.Body.STATIC)
-wall_left = pm.Body(body_type=pm.Body.STATIC)
-wall_right = pm.Body(body_type=pm.Body.STATIC)
-wall_bottom_shape = pm.Segment(
-    wall_bottom, (50, SCREEN_SIZE[1]-50), (SCREEN_SIZE[0]-50, SCREEN_SIZE[1]-50), 3)
-wall_left_shape = pm.Segment(
-    wall_left, (50, 100), (50, SCREEN_SIZE[1]-50), 3)
-wall_right_shape = pm.Segment(
-    wall_right, (SCREEN_SIZE[0]-50, 100), (SCREEN_SIZE[0]-50, SCREEN_SIZE[1]-50), 3)
-space.add(wall_bottom, wall_bottom_shape)
-space.add(wall_left, wall_left_shape)
-space.add(wall_right, wall_right_shape)
+            self.body.position = pos
+            self.shape = pm.Circle(body=self.body, radius=radius)
+            self.shape.elasticity = 0
+            self.shape.friction = 1
+            self.shape.color = self.color
+            self.space = space
+            self.space.add(self.body, self.shape)
 
-# preview ball init
-preview_ball = Ball(is_static=True)
-preview_ball.set_color(next_ball_color)
-preview_ball.set_visible()
+        def update(self):
+            self.rect.center = flipy(self.body.position)
+            # Use the body's angle to rotate the image.
+            self.image = pg.transform.rotozoom(
+                self.origin_image, math.degrees(self.body.angle), 1)
+            self.rect = self.image.get_rect(center=self.rect.center)
 
-balls: List[Ball] = []
-# endregion
+        def get_radius(self):
+            return self.shape.radius
 
-while running:
-    if game_over:
+        def get_color(self) -> pg.Color:
+            return self.color
+
+        def set_color(self, rgb: tuple):
+            self.color = pg.Color(rgb)
+            self.shape.color = self.color
+
+        def set_position(self, pos: tuple):
+            self.body.position = pos
+
+        def get_position(self) -> tuple:
+            return self.body.position
+
+    def __init__(self):
+        # Pygame Setting
+        pg.init()
+        self.screen = pg.display.set_mode(SCREEN_SIZE)
+        self.done = False
+        self.clock = pg.time.Clock()
+
+        # Suika Setting
+        self.game_over = False
+        self.is_ball_cooldown = False
+        self.last_create_time = 0
+        self.score = 0
+        self.ball_probability = [0.7, 0.3, 0, 0, 0, 0, 0, 0, 0]
+
+        self.balls: list[self.Ball] = []
+        self.ball_range = range(len(SCORE_ORDER))
+        self.random_int = random.choices(
+            self.ball_range, weights=self.ball_probability, k=1)[0]
+        self.next_ball_radius = RADIUS_ORDER[self.random_int]
+        self.next_ball_color = COLOR_ORDER[self.random_int]
+
+        # Pymunk stuff
+        self.space = pm.Space()
+        self.space.gravity = (0, 0)
+        # FEATURE?
+        # self.draw_options = pymunk.pygame_util.DrawOptions(self.screen)
+
+        self.all_sprites = pg.sprite.Group()
+
+        self.planet = self.Ball(
+            (SCREEN_SIZE[0]/2, SCREEN_SIZE[1]/2), RADIUS_ORDER[4], WHITE, self.space, True)
+
+        self.wall_bottom = pm.Body(body_type=pm.Body.STATIC)
+        self.wall_bottom_shape = pm.Segment(
+            self.wall_bottom, (50, SCREEN_SIZE[1]-50), (SCREEN_SIZE[0]-50, SCREEN_SIZE[1]-50), 3)
+        self.wall_left = pm.Body(body_type=pm.Body.STATIC)
+        self.wall_right = pm.Body(body_type=pm.Body.STATIC)
+        self.wall_left_shape = pm.Segment(
+            self.wall_left, (250, 100), (250, SCREEN_SIZE[1]-50), 3)
+        self.wall_right_shape = pm.Segment(
+            self.wall_right, (SCREEN_SIZE[0]-50, 100), (SCREEN_SIZE[0]-50, SCREEN_SIZE[1]-50), 3)
+        self.space.add(self.wall_left, self.wall_left_shape)
+        self.space.add(self.wall_right, self.wall_right_shape)
+        self.space.add(self.wall_bottom, self.wall_bottom_shape)
+
+    def run(self):
+        while not self.done:
+            self.dt = self.clock.tick(60) / 1000
+            if self.game_over:
+                self.show_exit_menu()
+            else:
+                self.handle_events()
+                self.run_logic()
+                self.check_collision()
+                self.check_end()
+                self.draw()
+                self.current_fps = self.clock.get_fps()
+
+        pg.display.quit()
+        pg.quit()
+        exit()
+
+    def show_exit_menu(self):
         font = pg.font.Font(None, 36)
         game_over_text_lines = [
             "GAME OVER",
-            f"Your Score is: {score}",
+            f"Your Score is: {self.score}",
             "restart by pressing SPACE",
             "quit game by pressing ESCAPE"
         ]
         for i, line in enumerate(game_over_text_lines):
             line_text = font.render(line, True, WHITE)
-            screen.blit(line_text, (SCREEN_SIZE[0] / 2 - line_text.get_width() / 2,
-                        SCREEN_SIZE[1] / 2 - line_text.get_height() / 2 + i * line_text.get_height()))
+            self.screen.blit(line_text, (SCREEN_SIZE[0] / 2 - line_text.get_width() / 2,
+                                         SCREEN_SIZE[1] / 2 - line_text.get_height() / 2 + i * line_text.get_height()))
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
-                running = False
+                self.done = True
             elif event.type == pg.KEYDOWN:
                 if event.key == pg.K_SPACE:
-                    remove_ball(*balls)
-                    balls.clear()
-                    score = 0
-                    game_over = False
+                    Game().run()
+                    self.balls.clear()
+                    self.score = 0
+                    self.game_over = False
                 elif event.key == pg.K_ESCAPE:
-                    running = False
-        space.debug_draw(draw_options)
-        pg.display.update()
-        continue
+                    self.done = True
+        pg.display.flip()
+        self.screen.fill(pg.Color((0, 0, 0, 200)))
 
-    screen.fill(BLACK)
-    clock.tick(FPS)
-    space.step(1/FPS)  # Physics framerate interval
+    def handle_events(self):
+        # Keyboard Event
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.done = True
+            elif event.type == pg.KEYDOWN and event.key == pg.K_BACKSPACE:
+                self.game_over = True
+        # Mouse Event
+        if pg.mouse.get_pressed()[0]:
+            current_time = pg.time.get_ticks()
+            is_cooldown = current_time - self.last_create_time < COOLDOWN_DURATION
+            if not is_cooldown:
+                self.last_create_time = current_time
+                is_cooldown = True
+                mouse_pos = pg.mouse.get_pos()
 
-    # 항상 다음 공 미리보기
-    preview_ball_coord_y = 20
-    preview_ball.set_color(next_ball_color)
-    preview_ball.set_position((pg.mouse.get_pos()[0], preview_ball_coord_y))
+                # 80 < X < SCREENSIZE.X - 80
+                x = max(80, min(mouse_pos[0], SCREEN_SIZE[0] - 80))
+                y = PREVIEW_BALL_OFFSET_Y
+                self.generate_ball(x, y)
 
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            running = False
-        elif event.type == pg.MOUSEBUTTONDOWN:
-            if event.pos[0] > 40 and event.pos[0] < SCREEN_SIZE[0] - 40:
-                # draw a ball when mouse is clicked
-                x = event.pos[0]
-                y = min(event.pos[1], 30)
-                ball = Ball(pos=(x, y), radius=next_ball_radius)
-                ball.set_color(next_ball_color)
-                ball.set_position((x, y))
-                ball.set_visible()
-                balls.append(ball)
-                print(f"current {len(balls)} balls")
-                # random setting
                 next_index = random.choices(
-                    random_index, weights=skwed_probabitity, k=1)[0]
-                next_ball_color = order_color[next_index]
-                next_ball_radius = order_radius[next_index]
-                # refresh preview ball
-                remove_ball(preview_ball)
-                preview_ball = Ball(pos=(pg.mouse.get_pos()[0], 30),
-                                    radius=next_ball_radius, is_static=True)
-                preview_ball.set_visible()
+                    self.ball_range, weights=self.ball_probability, k=1)[0]
+                self.next_ball_color = COLOR_ORDER[next_index]
+                self.next_ball_radius = RADIUS_ORDER[next_index]
 
-    # check current position is valid
-    for ball in balls:
-        pos = ball.get_position()
-        if pos[0] < 0 or pos[0] > SCREEN_SIZE[0] or pos[1] < 0 or pos[1] > SCREEN_SIZE[1]:
-            remove_ball(ball)
-            game_over = True
-            print('TEST: GAME OVER')
-            continue
+    def run_logic(self):
+        self.space.step(1/60)
+        self.all_sprites.update()
 
-    for i in range(len(balls)):
-        # if ball removed, escape all of for loops
-        need_to_escape = False
-        for j in range(i+1, len(balls)):
-            pointSet = balls[i].shape.shapes_collide(balls[j].shape)
-            is_hit = len(pointSet.points) != 0
-            # hit and level is same
-            if is_hit and balls[i].shape.radius == balls[j].shape.radius:
-                next_level_index = get_next_radius(balls[i].shape.radius)
-                # if level != max
-                if next_level_index != -1:
-                    target_pos = ((balls[i].get_position()[0] + balls[j].get_position()[0])/2,
-                                  (balls[i].get_position()[1] + balls[j].get_position()[1])/2)
-                    b = Ball(target_pos, radius=order_radius[next_level_index])
-                    b.set_color(order_color[next_level_index])
-                    b.set_visible()
-                    balls.append(b)
-                remove_ball(balls[i], balls[j])
-                need_to_escape = True
-                break
-        if need_to_escape:
-            break
+    def change_probability(self):
+        if self.score >= 4 and self.score < 12:
+            self.ball_probability = [0.55, 0.4, 0.05, 0, 0, 0, 0, 0, 0]
+        elif self.score >= 12 and self.score < 32:
+            self.ball_probability = [0.35, 0.35, 0.25, 0.05, 0, 0, 0, 0, 0]
+        elif self.score >= 32 and self.score < 128:
+            self.ball_probability = [0.2, 0.35, 0.25, 0.15, 0.05, 0, 0, 0, 0]
+        elif self.score >= 128:
+            self.ball_probability = [0.1, 0.35, 0.27, 0.18, 0.1, 0, 0, 0, 0]
+        return
 
-    # draw color
-    colors_coord_y = SCREEN_SIZE[1] - 20
-    colors_each_space = 40
-    for i, color in enumerate(order_color):
-        pg.draw.circle(
-            screen, color, (40 + i * colors_each_space, colors_coord_y), 10)
+    def check_collision(self):
+        # if ball merged -> return
+        for i in range(len(self.balls)):
+            for j in range(i+1, len(self.balls)):
+                ball_i, ball_j = self.balls[i], self.balls[j]
+                pointSet = ball_i.shape.shapes_collide(ball_j.shape)
+                is_hit = len(pointSet.points) != 0  # 접점
+                # hit and level is same
+                if is_hit and ball_i.get_radius() == ball_j.get_radius():
+                    next_index = get_next_radius(ball_i.get_radius())
+                    self.change_probability()
+                    # if level is NOT max generate new ball
+                    if next_index != -1:
+                        target_pos = ((ball_i.get_position()[0] + ball_j.get_position()[0])/2,
+                                      (ball_i.get_position()[1] + ball_j.get_position()[1])/2)
+                        new_ball = self.Ball(
+                            target_pos, RADIUS_ORDER[next_index], COLOR_ORDER[next_index], self.space)
+                        self.balls.append(new_ball)
+                        self.score += SCORE_ORDER[next_index-1]
+                    else:
+                        self.score += SCORE_ORDER[-1]
+                    self.remove_ball(ball_i, ball_j)
+                    return
 
-    # draw score
-    font = pg.font.Font(None, 36)
-    score_text = font.render(f'Score: {score}', True, WHITE)
-    screen.blit(score_text, (SCREEN_SIZE[0] / 2 -
-                score_text.get_width()/2, 20))
-    space.debug_draw(draw_options)
-    pg.display.update()
-    continue
+    def draw(self):
+        self.screen.fill(pg.Color(140, 120, 110))
+        self.all_sprites.draw(self.screen)  # Draw the images of all sprites.
+
+        pg.draw.circle(self.screen, self.planet.get_color(),
+                       self.planet.get_position(), self.planet.get_radius())
+
+        mouse_x = max(80, min(pg.mouse.get_pos()[0], SCREEN_SIZE[1] - 80))
+        pg.draw.circle(self.screen, self.next_ball_color,
+                       (mouse_x, PREVIEW_BALL_OFFSET_Y), self.next_ball_radius)
+
+        for ball in self.balls:
+            pg.draw.circle(self.screen, ball.get_color(),
+                           ball.get_position(), ball.get_radius())
+
+        # color order draw
+        colors_coord_y = SCREEN_SIZE[1] - 20
+        colors_each_space = 40
+        for i, color in enumerate(COLOR_ORDER):
+            pg.draw.circle(
+                self.screen, color, (40 + i * colors_each_space, colors_coord_y), 10)
+
+        # draw score
+        font = pg.font.Font(None, 36)
+        score_text = font.render(f'Score: {self.score}', True, WHITE)
+        self.screen.blit(score_text, (SCREEN_SIZE[0] / 2 -
+                                      score_text.get_width()/2, 20))
+        # bottom, left, right
+        pg.draw.line(self.screen, WHITE,  (50,
+                     SCREEN_SIZE[1]-50), (SCREEN_SIZE[0]-50, SCREEN_SIZE[1]-50), 3)
+        pg.draw.line(self.screen, WHITE, (250, 100),
+                     (250, SCREEN_SIZE[1]-50), 3)
+        pg.draw.line(self.screen, WHITE,
+                     (SCREEN_SIZE[0]-50, 100), (SCREEN_SIZE[0]-50, SCREEN_SIZE[1]-50), 3)
+
+        pg.display.flip()
+
+    def check_end(self):
+        for ball in self.balls:
+            if ball.shape.shapes_collide(self.wall_left_shape).points or \
+                    ball.shape.shapes_collide(self.wall_right_shape).points or \
+                    ball.shape.shapes_collide(self.wall_bottom_shape).points:
+                self.game_over = True
+
+    def generate_ball(self, x, y):
+        ball = self.Ball((x, y), self.next_ball_radius,
+                         self.next_ball_color, self.space)
+        ball.set_color(self.next_ball_color)
+        ball.set_position((x, y))
+        self.balls.append(ball)
+
+    def remove_ball(self, *args):
+        for ball in args:
+            if ball in self.balls:
+                self.balls.remove(ball)
+            self.space.remove(ball.shape, ball.body)
+
+    def custom_gravity(body: pm.Body, gravity, damping, dt):
+        current_coord = body.position
+        target_vec = ((SCREEN_SIZE[0]/2 - current_coord[0])*2,
+                      (SCREEN_SIZE[1]/2 - current_coord[1])*2)
+        pymunk.Body.update_velocity(body, target_vec, damping, dt)
 
 
-pg.quit()
+if __name__ == '__main__':
+    Game().run()
